84f86fdc58a69b5f0683adb0290508892a18ec87
dgr92 <david-gr_92@hotmail.com>
2024-11-14 21:33:23 +0100

    creado repo

---
+++ primer-programa/PROG01_programa1.java
@@ -0,0 +1,14 @@
+class PROG01_programa1 {
+    public static void main(String[] args) {
+        System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01");
+        System.out.println("Introducción a la programación");
+        System.out.println("David Graciá Requena");
+        System.out.println("Caudete (Albacete)");
+        System.out.println("21:53 - 27/10/2024");
+        System.out.println("Programa1");
+    }
+}
+
+
+
+
