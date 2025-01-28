f2a3a81c4efa66251e2517b79da99c21a3ff6c5e
dgr92 <david-gr_92@hotmail.com>
2025-01-28 20:47:02 +0100

    continuado ejercicio ut5

--- UT5/PROG05_Tarea/src/main/java/com/prog05_ejerc1/Libro.java
+++ UT5/PROG05_Tarea/src/main/java/com/prog05_ejerc1/Libro.java
@@ -1,8 +1,98 @@
 package com.prog05_ejerc1;
+import java.util.Date;
+import java.time.Year;
 
 /**
  * @author David
  */
 public class Libro {
+  private String titulo;
+  private String autor;
+  private long isbn;
+  private int numPags;
+  private String descripcion;  
+  private Date anioPublicacion;
+  private int numEdicion;
   
+  
+  // Constructor por defecto
+  protected Libro(){};
+  
+  // Constructor con valores
+  protected Libro(String titulo, String autor, long isbn, int numPags, String descripcion, Date anioPublicacion, int numEdicion){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.isbn = isbn;
+    this.numPags = numPags;
+    this.descripcion = descripcion;
+    this.anioPublicacion = anioPublicacion;    
+    this.numEdicion = numEdicion;
+  };
+  
+  
+  
+  // Getters
+  protected String getTitulo(){
+    return titulo;
+  };
+  
+  protected String getAutor(){
+    return autor;
+  };
+  
+  protected long getIsbn(){
+    return isbn;
+  };
+  
+  protected int getNumPags(){
+    return numPags;
+  }
+  
+  protected String getDescripcion(){
+    return descripcion;
+  }
+  
+  protected int getNumEdicion(){
+    return numEdicion;
+  };
+  
+  protected Date getFechaPublicacion(){
+    return anioPublicacion;
+  };
+  
+  protected Year getAntiguedad(){
+    Year anioActual = Year.now();
+    System.out.println(anioActual);
+    return anioActual;
+  }
+  
+  
+  // Setters 
+  protected void setTitulo(String titulo){
+    this.titulo = titulo;
+  };
+  
+  protected void setAutor(String autor){
+    this.autor = autor;
+  };
+  
+  protected void setIsbn(long isbn){
+    this.isbn = isbn;
+  };
+  
+  protected void setNumPags(int numPags){
+    this.numPags = numPags;
+  }
+  
+  protected void setDescripcion(String descripcion){
+    this.descripcion = descripcion;
+  }
+  
+  protected void setNumEdicion(int numEdicion){
+     this.numEdicion = numEdicion;
+  };
+  
+  protected void setAnioPublicacion(Date anioPublicacion){
+    this.anioPublicacion = anioPublicacion;
+  };
 }
--- UT5/PROG05_Tarea/src/main/java/com/prog05_ejerc1/Principal.java
+++ UT5/PROG05_Tarea/src/main/java/com/prog05_ejerc1/Principal.java
@@ -7,12 +7,16 @@ import com.prog05_ejerc1.Libro;
  */
 public class Principal {
 
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      
+  public static void main(String[] args) {
+    boolean exit = false;
+    Libro libro = new Libro();
+
+    Scanner sc = new Scanner(System.in);
+
+    while (!exit) {        
       System.out.println("\n------------------------------------");
       System.out.println("Selecciona que hacer:\n");
-      
+
       System.out.println("1. Nuevo Libro.");
       System.out.println("2. Ver Título.");
       System.out.println("3. Ver Autor.");
@@ -25,49 +29,53 @@ public class Principal {
       System.out.println("10. Salir.");
       System.out.println("------------------------------------\n\n");
       
+      
       switch (sc.nextInt()) {
         case 1:
-          System.out.println(1);
+          libro = new Libro("a", "b", 456L, 4, "asf", , 4);
+
           break;
         
         case 2:
-          System.out.println(2);
+          System.out.println(libro.getTitulo());
           break;
         
         case 3:
-          System.out.println(3);
+          System.out.println("autor");
           break;
         
         case 4:
-          System.out.println(4);
+          System.out.println("isbn");
           break;
         
         case 5:
-          System.out.println(5);
+          System.out.println("ediciones");
           break;
         
         case 6:
-          System.out.println(6);
+          System.out.println("actualizando ediciones");
           break;
         
         case 7:
-          System.out.println(7);
+          System.out.println("fecha public");
           break;
         
         case 8:
-          System.out.println(8);
+          System.out.println("descripcion");
           break;
         
         case 9:
-          System.out.println(9);
+          System.out.println("antiguedad");
           break;
         
         case 10:
-          System.out.println(10);
+          exit = true;
+          System.out.println("Finalizando programa.");
           break;
           
         default:
           throw new AssertionError();
       }
     }
+  }
 }
