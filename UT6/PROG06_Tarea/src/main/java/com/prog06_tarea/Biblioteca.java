d52bf8c655e912fa7a90cd8f4a31183c3170597c
dgr92 <david-gr_92@hotmail.com>
2025-02-11 23:51:02 +0100

    continuado ejercicio 6

--- UT6/PROG06_Tarea/src/main/java/com/prog06_tarea/Biblioteca.java
+++ UT6/PROG06_Tarea/src/main/java/com/prog06_tarea/Biblioteca.java
@@ -1,5 +1,4 @@
 package com.prog06_tarea;
-import com.prog06_tarea.Libro;
 import java.util.ArrayList;
 
 public class Biblioteca {
@@ -27,18 +26,62 @@ public class Biblioteca {
     }
 
 
-    // protected String buscaLibro(long isbn){
+    protected String buscaLibro(long isbn){
+      for(int i= 0; i < librosBiblioteca.size(); i++){
+        if(librosBiblioteca.get(i).getIsbn() == isbn){
+          return "Título: " + librosBiblioteca.get(i).getTitulo() + "\n" +
+                 "Autor: " + librosBiblioteca.get(i).getAutor() + "\n" +
+                 "ISBN: " + librosBiblioteca.get(i).getIsbn() + "\n" +
+                 "Descripción: " + librosBiblioteca.get(i).getDescripcion() + "\n" +
+                 "Número de páginas: " + librosBiblioteca.get(i).getNumPags() + "\n" +
+                 "Año de publicación: " + librosBiblioteca.get(i).getAnioPublicacion() + "\n" +
+                 "Número de edición: " + librosBiblioteca.get(i).getNumEdicion() + "\n";
+        }
+      } 
+      return "----------------------------------------\n" + 
+             "No existe libro con el ISBN introducido.\n" + 
+             "----------------------------------------";
+    }
 
-    // }
 
+    protected void listaLibros(){
+      if(librosBiblioteca.size() == 0){
+        System.out.println("No hay libros en la biblioteca.");
+        System.out.println("-------------------------------");
 
-    // protected void listaLibros(){
-    //     for (int i = 0; i < librosBiblioteca.length; i++) {
+      }
 
-    //     }
-    // }
+      for (int i = 0; i < librosBiblioteca.size(); i++) {
+        System.out.println("Libro nº" + (i+1) + "\n");
+        System.out.println("Titulo: \n" + librosBiblioteca.get(i).getTitulo() + "\n");
+        System.out.println("Autor: \n" + librosBiblioteca.get(i).getAutor() + "\n");
+        System.out.println("ISBN: \n" + librosBiblioteca.get(i).getIsbn() + "\n");
+        System.out.println("Descripcion: \n" + librosBiblioteca.get(i).getDescripcion() + "\n");
+        System.out.println("Numero de paginas: \n" + librosBiblioteca.get(i).getNumPags() + " páginas.\n");
+        System.out.println("Año de publicacion: \n" + librosBiblioteca.get(i).getAnioPublicacion() + "\n");
+        System.out.println("Numero de edicion: \n" + librosBiblioteca.get(i).getNumEdicion() + "\n");
+        System.out.println("-------------------------------------------------");
+      }
+    }
     
-    // protected boolean actualizaEdiciones(){
+    
+    protected boolean actualizaEdiciones(long isbn, int numEdicion){
+      for (int i = 0; i < librosBiblioteca.size(); i++) {
+        if(librosBiblioteca.get(i).getIsbn() == isbn){
+          librosBiblioteca.get(i).setNumEdicion(numEdicion);
+          return true;
+        }
+      }
+      return false;
+    }
 
-    // }
+    protected boolean eliminarLibro(long isbn){
+      for (int i = 0; i < librosBiblioteca.size(); i++) {
+        if(librosBiblioteca.get(i).getIsbn() == isbn){
+          librosBiblioteca.remove(i);
+          return true;
+        }
+      }
+      return false;
+    }
 }
--- UT6/PROG06_Tarea/src/main/java/com/prog06_tarea/Principal.java
+++ UT6/PROG06_Tarea/src/main/java/com/prog06_tarea/Principal.java
@@ -1,5 +1,4 @@
 package com.prog06_tarea;
-import com.prog06_tarea.Biblioteca;
 import java.util.Calendar;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -25,7 +24,7 @@ public class Principal {
     int numPags = 0;
     int anioPublicacion = 0;
     int numEdicion = 1;
-    int nuevoNumEdicion;
+
     String isbnString = "";
     String regexIsbn = "(\\d{13})";
     Pattern patternIsbn = Pattern.compile(regexIsbn);
@@ -43,10 +42,11 @@ public class Principal {
       System.out.println("Selecciona que hacer:\n");
 
       System.out.println("1. Nuevo Libro.");
-      System.out.println("2. Listar Libro.");
+      System.out.println("2. Listar Libros.");
       System.out.println("3. Buscar Libro.");
       System.out.println("4. Modificar ediciones Libro.");
-      System.out.println("5. Salir.");
+      System.out.println("5. Eliminar Libro.");
+      System.out.println("6. Salir.");
       System.out.println("------------------------------------\n\n");
       
       
@@ -62,9 +62,9 @@ public class Principal {
           matcherDni = patternDni.matcher(dniBibliotecario);
 
           while (!matcherDni.matches()) { // Bucle que se repite mientras que el DNI introducido no sea válido
-            System.out.println("-----------------------------------------");
+            System.out.println("----------------------------------------");
             System.out.println("ERROR!! El DNI introducido no es válido.");
-            System.out.println("-----------------------------------------");
+            System.out.println("----------------------------------------");
             System.out.print("\nDNI Bibliotecario: "); 
             dniBibliotecario = sc.nextLine(); 
             matcherDni = patternDni.matcher(dniBibliotecario);
@@ -75,9 +75,9 @@ public class Principal {
           String titulo = sc.nextLine(); 
 
           while(titulo.length() == 0 || titulo.length() > 40){ // Bucle que se repite mientras que el título introducido no sea válido
-            System.out.println("----------------------------------------------------------------------");
+            System.out.println("-----------------------------------------------------------------------");
             System.out.println("ERROR!! El título no puede estar vacío o contener más de 40 caracteres.");
-            System.out.println("----------------------------------------------------------------------");
+            System.out.println("-----------------------------------------------------------------------");
             
             System.out.print("\nTítulo: "); 
             titulo = sc.nextLine(); 
@@ -98,36 +98,35 @@ public class Principal {
               matcherIsbn = patternIsbn.matcher(isbnString);
 
               if(!matcherIsbn.matches()){ // Controla que el ISBN introducido sea válido
-                System.out.println("-------------------------------------------------");
+                System.out.println("-----------------------------------------");
                 System.out.println("ERROR!! El ISBN introducido no es válido.");
-                System.out.println("-------------------------------------------------");
+                System.out.println("-----------------------------------------");
               }
               
             } catch (InputMismatchException e) {
-              System.out.println("-------------------------------------------------");
+              System.out.println("-----------------------------------");
               System.out.println("ERROR!! El ISBN debe ser un número.");
-              System.out.println("-------------------------------------------------");   
+              System.out.println("-----------------------------------");   
               sc.next(); // Limpia el buffer
             }
 
           } while (!matcherIsbn.matches());
 
           
-
           do { // Bucle que se repite mientras que el número de páginas introducido no sea válido
             System.out.print("\nNúmero de Páginas: ");
             try {
               numPags = sc.nextInt(); 
               sc.nextLine(); // Limpia el buffer 
               if(numPags <= 0){ // Controla que el número de páginas sea mayor que 0
-                System.out.println("-------------------------------------------------");
+                System.out.println("--------------------------------------------------");
                 System.out.println("ERROR!! El Número de paginas debe ser mayor que 0.");
-                System.out.println("-------------------------------------------------");
+                System.out.println("--------------------------------------------------");
               }
             } catch (InputMismatchException e) { // Controla que el número de páginas sea un número
-              System.out.println("-------------------------------------------------");
+              System.out.println("------------------------------------------------");
               System.out.println("ERROR!! El Número de paginas debe ser un número.");
-              System.out.println("-------------------------------------------------");
+              System.out.println("------------------------------------------------");
               sc.nextLine(); // Limpia el buffer
             }
           } while (numPags <= 0); 
@@ -143,9 +142,9 @@ public class Principal {
             sc.nextLine(); // Limpia el buffer
 
           } catch (InputMismatchException e) { // Controla que el año de publicación sea un número
-            System.out.println("-------------------------------------------------");
+            System.out.println("------------------------------------------------");
             System.out.println("ERROR!! El Año de publicación debe ser numérico."); 
-            System.out.println("-------------------------------------------------");
+            System.out.println("------------------------------------------------");
             sc.nextLine(); // Limpia el buffer
           }
 
@@ -161,9 +160,9 @@ public class Principal {
               sc.nextLine(); // Limpia el buffer
 
             } catch (InputMismatchException e) { // Controla que el año de publicación sea un número
-              System.out.println("-------------------------------------------------");
+              System.out.println("------------------------------------------------");
               System.out.println("ERROR!! El Año de publicación debe ser numérico."); 
-              System.out.println("-------------------------------------------------");
+              System.out.println("------------------------------------------------");
               sc.nextLine(); // Limpia el buffer
             }
           }
@@ -186,61 +185,80 @@ public class Principal {
           int estadoNuevoLibro = biblioteca.insertarLibro(titulo, autor, isbn, numPags, descripcion, anioPublicacion, numEdicion);
 
           if(estadoNuevoLibro == -1){
-            System.out.println("\n----------------------------------------------------------------");
+            System.out.println("\n---------------------------------------------------------------");
             System.out.println("ERROR!! La biblioteca está llena, no se pueden añadir más libros.");
-            System.out.println("------------------------------------------------------------------");
+            System.out.println("-----------------------------------------------------------------");
           
           } else if(estadoNuevoLibro == -2){
-            System.out.println("\n-------------------------------------------------------");
+            System.out.println("\n-----------------------------------------------------");
             System.out.println("ERROR!! El ISBN introducido ya existe en la biblioteca.");
-            System.out.println("---------------------------------------------------------");
+            System.out.println("-------------------------------------------------------");
           
           } else if(estadoNuevoLibro == 0){
-            System.out.println("\n--------------------------------");
+            System.out.println("\n------------------------------");
             System.out.println("El libro se ha creado con éxito.");
-            System.out.println("------------------------------------");
+            System.out.println("--------------------------------");
           }
           
           break;
 
-        // case 2: // Muestra el título del libro
-        //   // Se comprueba que el libro existe, si no existe, muestra un mensaje de error y vuelve al menú, ésto se repite en todas las opciones salvo la 1.
-        //   if(!Validaciones.existeLibro(libro.getTitulo())){
-        //     break;
-        //   }
-        //   System.out.println("2. Listar Libro.");
-        //   System.out.println("--------------\n");
-        //   System.out.println("El título es: " + libro.getTitulo());
-        //   break;
+        case 2: // Mostrar listado de libros
+          System.out.println("2. Listar Libros.");
+          System.out.println("------------------------------------------");
+          biblioteca.listaLibros();
+          break;
               
 
-        // case 3: // Muestra el ISBN del libro
-        //   if(!Validaciones.existeLibro(libro.getTitulo())){
-        //     break;
-        //   }
-        //   System.out.println("4. Buscar Libro.");
-        //   System.out.println("------------\n");
-        //   System.out.println("El ISBN es: " + libro.getIsbn());
-        //   break;
+        case 3: // Muestra el ISBN del libro
+          System.out.println("3. Buscar Libro.");
+          System.out.println("------------------------------------------");
+          System.out.println(biblioteca.buscaLibro(sc.nextLong()));
+          sc.nextLine(); // Limpia el buffer
+          break;
         
 
-        // case 4: // Actualiza el número de ediciones del libro
-        //   if(){
-        //     break;
-        //   }
-        //   System.out.println("4. Modificar ediciones Libro.");
-        //   System.out.println("---------------------------------\n");
-        //   do { // Bucle que, si el número introducido no es válido, se repite hasta que lo sea
-        //     System.out.print("Introduce el nuevo Número de Edición: ");
-        //     nuevoNumEdicion = sc.nextInt();
-        //   } while (!Validaciones.actualizarNumEdicion(numEdicion, nuevoNumEdicion)); // Llama al método de Validaciones para comprobar que el nuevo número de edicion es válido
+        case 4: // Actualiza el número de ediciones del libro
+        System.out.println("4. Modificar ediciones Libro.");
+        System.out.println("---------------------------------\n");
+        System.out.print("Introduce el ISBN del libro: ");
+        isbn = sc.nextLong();
+        sc.nextLine(); // Limpia el buffer de entrada
+        System.out.print("Introduce el nuevo número de ediciones: ");
+        int nuevoNumEdicion = sc.nextInt();
+        sc.nextLine(); // Limpia el buffer de entrada
 
-        //   libro.setNumEdicion(nuevoNumEdicion);
-        //   System.out.println("\nEdición cambiada de la " + numEdicion + " a la " + libro.getNumEdicion());
-        //   break;
+        if(biblioteca.actualizaEdiciones(isbn, nuevoNumEdicion)){
+          System.out.println("\n-------------------------------------------------------");
+          System.out.println("El número de ediciones se ha actualizado correctamente.");
+          System.out.println("-------------------------------------------------------\n");
+        } else {
+          System.out.println("\n---------------------------------------------------");
+          System.out.println("ERROR!! No se ha encontrado el libro con ese ISBN.");
+          System.out.println("---------------------------------------------------\n");
+        }
+        break;
         
+        case 5: // Eliminar libro
+          System.out.println("5. Eliminar libro.");
+          System.out.println("---------------------------------\n");
+          System.out.print("Introduce el ISBN del libro a eliminar: ");
+          isbn = sc.nextLong();
+          sc.nextLine(); // Limpia el buffer de entrada
+          
+          if (biblioteca.eliminarLibro(isbn)) {
+            System.out.println("\n---------------------------------");
+            System.out.println("El libro se ha eliminado con éxito.");
+            System.out.println("---------------------------------\n");
+          } else {
+            System.out.println("\n-----------------------------------");
+            System.out.println("ERROR!! No se ha encontrado el libro.");
+            System.out.println("-----------------------------------\n");
+          }
 
-        case 5: // Sale del programa
+        break;
+
+
+        case 6: // Sale del programa
           exit = true; // Cambia el valor de la variable de control para salir del bucle del menú
           sc.close(); // Cierra el Scanner
           System.out.println("\n-----------------------");
